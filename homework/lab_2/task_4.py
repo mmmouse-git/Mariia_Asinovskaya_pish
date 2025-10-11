c3afbf9c2153f017b3e2cee2e075a0a82da40e32
Maria Asinovskaya <m.asinovskaya@gmail.com>
2025-10-11 12:47:23 +0300

    task_4 done

---
+++ homework/lab_2/task_4.py
@@ -0,0 +1,11 @@
+'''Составить функцию, которая определяет сумму цифр в числе'''
+
+def summ(number):
+    result = 0
+    while number > 0:
+        result = result + number % 10
+        number = number // 10
+    return result
+
+
+print(summ(123))
